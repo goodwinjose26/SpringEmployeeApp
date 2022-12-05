a7ba2eb40e76f052efe8cb1932fc6a877cf15536
goodwinjose26 <josegoodwin22@gmail.com>
2022-12-05 15:56:13 +0530

    edit route

--- src/main/java/com/example/employee_backend/controller/EmployeeController.java
+++ src/main/java/com/example/employee_backend/controller/EmployeeController.java
@@ -28,4 +28,10 @@ public class EmployeeController {
     {
         return "SEARCH EMPLOYEE ";
     }
+
+    @GetMapping("/edit")
+    public String edit()
+    {
+        return "EDIT EMPLOYEE ";
+    }
 }
