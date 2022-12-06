a433b980f24df15002fcc578e5aea6741bdf21db
goodwinjose26 <josegoodwin22@gmail.com>
2022-12-06 15:21:49 +0530

    view

--- src/main/java/com/example/employee_backend/controller/EmployeeController.java
+++ src/main/java/com/example/employee_backend/controller/EmployeeController.java
@@ -1,13 +1,19 @@
 package com.example.employee_backend.controller;
 
+import com.example.employee_backend.dao.EmployeeDao;
 import com.example.employee_backend.model.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class EmployeeController {
+    @Autowired
+    private EmployeeDao dao;
 
 
     @PostMapping(path = "/add",consumes = "application/json",produces = "application/json")
@@ -20,7 +26,14 @@ public class EmployeeController {
         System.out.println(e.getName().toString());
         System.out.println(e.getPassword().toString());
         System.out.println(e.getSalary());
+        dao.save(e);
         return "ADD EMPLOYEE DETAILS";
+    }
+
+    @GetMapping("/view")
+    public List<Employee> view()
+    {
+        return  (List<Employee>) dao.findAll();
     }
 
 
